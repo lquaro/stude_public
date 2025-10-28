f2605f843184340d6b8d963759bf0a4f1c73fb55
lquaro <eelonjoy@gmail.com>
2025-10-28 19:12:07 +0300

    пушу третью лабу

---
+++ AIS/lab3/blog/articles/views.py
@@ -0,0 +1,7 @@
+from .models import Article
+from django.shortcuts import render
+from django import template
+from django.http import HttpResponse
+
+def archive(request):
+    return render(request, 'templates/archive.html', {"posts": Article.objects.all()})
--- AIS/lab3/blog/blog/urls.py
+++ AIS/lab3/blog/blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for firstwebpage project.
+URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from flatpages import views
+from articles import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path( 'hello/', views.home ),
+    path('', views.archive, name='archive'),
 ]
