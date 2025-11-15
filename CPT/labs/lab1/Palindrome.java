c45c810b151cafe9f29f2ffdf8f602a686b6b851
lquaro <eelonjoy@gmail.com>
2025-11-15 11:44:56 +0300

    push lab1

---
+++ CPT/labs/lab1/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    public static String reverseString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            System.out.println(s + " -> " + (isPalindrome(s) ? "palindrome" : "not palindrome"));
+        }
+    }
+}
