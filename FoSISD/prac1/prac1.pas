472335660acb7b452f3e4a9beb36340538ed968e
lquaro <eelonjoy@gmail.com>
2025-11-15 17:04:46 +0300

    пуш дисциплины СИАОД

---
+++ FoSISD/prac1/prac1.pas
@@ -0,0 +1,34 @@
+program SqrtApprox;
+var
+  n, x, step: real;
+begin
+  readln(n);
+  if n < 0 then
+    writeln('Error: n < 0')
+  else
+  begin
+    x := 0.0;
+
+    step := 1.0;
+    while (x + step) * (x + step) <= n do
+      x := x + step;
+
+    step := 0.1;
+    while (x + step) * (x + step) <= n do
+      x := x + step;
+
+    step := 0.01;
+    while (x + step) * (x + step) <= n do
+      x := x + step;
+
+    step := 0.001;
+    while (x + step) * (x + step) <= n do
+      x := x + step;
+
+    step := 0.0001;
+    while (x + step) * (x + step) <= n do
+      x := x + step;
+
+    writeln(x:0:4);
+  end;
+end.
